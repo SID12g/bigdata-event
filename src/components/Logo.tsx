6492cc1a2575ce4783a6f7fb57a3af4321f27cd5
sid12g <sead12g@gmail.com>
2026-04-23 19:13:35 +0900

    feat: add Logo Component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+export default function Logo() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center flex px-[16px] py-[14px] mt-[80px] bg-[var(--color-brown-bg)] border-[2px] border-[var(--color-brown-border)] border-[8px]">
+      <p className="text-[24px] mb-[4px] text-[var(--color-yellow-light)]">
+        BIG DATA
+      </p>
+      <p className="text-[40px] text-[var(--color-yellow-primary)]">
+        빅데이터 퀴즈
+      </p>
+    </div>
+  );
+}
