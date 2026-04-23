29dfbd95776fceb5b5e4efeebaafc35f53d32a2a
sid12g <sead12g@gmail.com>
2026-04-24 02:58:59 +0900

    feat: add allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    process.env.ALLOW_DEV_ORIGIN ? process.env.ALLOW_DEV_ORIGIN : "",
+  ],
 };
 
 export default nextConfig;
