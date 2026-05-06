7401f6efedcb982ba1c51c8ea189146e81b53ec5
sid12g <sead12g@gmail.com>
2026-05-06 16:00:05 +0900

    fix: image crop type

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -4,24 +4,13 @@ import BackgroundImage from "@/../public/images/background.png";
 export default function Background() {
   return (
     <div>
-      <div className="fixed inset-0 z-[-10] flex w500:hidden overflow-hidden">
+      <div className="fixed inset-0 z-[-10] flex overflow-hidden">
         <Image
           src={BackgroundImage}
           alt="Background Image"
           fill
           loading="eager"
           style={{ objectFit: "cover", objectPosition: "center" }}
-        />
-      </div>
-
-      {/* 500px 이상: 원본 크기로 중앙 정렬 */}
-      <div className="fixed inset-0 z-[-10] hidden w500:flex items-center justify-center overflow-hidden">
-        <Image
-          src={BackgroundImage}
-          alt="Background Image"
-          loading="eager"
-          style={{ width: "auto", height: "100vh" }}
-          sizes="fill"
         />
       </div>
     </div>
