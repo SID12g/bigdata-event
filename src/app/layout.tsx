ffb8a9938ee364db0e34a592b400c058480f1e25
sid12g <sead12g@gmail.com>
2026-05-06 15:49:42 +0900

    fix: remove noscript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,23 +28,8 @@ export default function RootLayout({
   return (
     <html lang="ko" className={`${DnfBitBitSans.variable} h-full antialiased`}>
       <body className="flex items-center justify-center">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P2T82845"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <div className="landscape-lock" />
         {children}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P2T82845');`,
-          }}
-        />
       </body>
     </html>
   );
